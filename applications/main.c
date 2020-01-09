0a5b234b5c2607b531c6df099a6cd29be74feab1
Wendal Chen <wendal1985@gmail.com>
2020-01-09 14:06:43 +0800

    remove: 清除main.c里面的多余注释

--- applications/main.c
+++ applications/main.c
@@ -15,22 +15,8 @@
 
 int main(void)
 {
-    /* set wifi work mode */
-    //rt_wlan_set_mode(RT_WLAN_DEVICE_STA_NAME, RT_WLAN_STATION);
-    //rt_wlan_set_mode(RT_WLAN_DEVICE_AP_NAME, RT_WLAN_AP);
-    //rt_thread_mdelay(5000);
     luat_main(NULL, NULL, NULL);
-    //rt_pin_mode(14, PIN_MODE_OUTPUT);
-    //rt_pin_mode(15, PIN_MODE_OUTPUT);
     while (1) {
-        /*
-        rt_pin_write(14, 0);
-        rt_pin_write(15, 0);
-        rt_thread_mdelay(2000);
-        rt_pin_write(14, 1);
-        rt_pin_write(15, 1);
-        rt_thread_mdelay(2000);
-        */
         rt_thread_mdelay(2000000);
     }
     return 0;
